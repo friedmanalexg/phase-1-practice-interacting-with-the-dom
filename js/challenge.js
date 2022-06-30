d8bbac729186af82a37415bb5f2b2e4704e08d26
Alex <friedmanalexg@gmail.com>
2022-06-30 10:54:32 -0400

    event listeners test

---
+++ js/challenge.js
@@ -0,0 +1,12 @@
+// Hell yeah, get some, lol
+
+const counter = document.getElementById("counter");
+
+//event listener for click on minus
+document.getElementById("minus").addEventListener("click",() => counter--);
+//event listener for click on plus
+document.getElementById("plus").addEventListener("click",() => counter++);
+
+document.getElementById("heart").addEventListener
+document.getElementById("pause").addEventListener
+
