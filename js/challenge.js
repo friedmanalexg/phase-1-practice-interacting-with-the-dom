a88ee071797277fb8abe4bae0411c8a39e6f06c1
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-30 12:36:12 -0500

    Unfinished solution, new branch to avoid conflict with Alex's code

--- js/challenge.js
+++ js/challenge.js
@@ -1,18 +1,73 @@
 // Hell yeah, get some, lol
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const heart = document.getElementById("heart");
+const commentsContainer = document.getElementById("list");
+const form = document.getElementById("comment-form");
 
-const counter = document.getElementById("counter");
+// ----Plus button functionality----
+function increment() {
+  const currentCounterAsAString = document.getElementById("counter");
+  const counter = parseInt(currentCounterAsAString.innerText);
+  currentCounterAsAString.innerText = counter + 1;
+}
 
-
+plus.addEventListener("click", function() {
+  increment();
+});
 
 //event listener for click on minus
-minus.addEventListener("click",function(){var a=document.getElementById("counter"),counterVal=parseInt(a.innerText);
-a.innerText=counterVal-1});
+minus.addEventListener("click", function() {
+  var a=document.getElementById("counter")
+  counterVal=parseInt(a.innerText);
+  a.innerText=counterVal-1
+});
 
-//event listener for click on plus
-plus.addEventListener("click",function(){var a=document.getElementById("counter"),counterVal=parseInt(a.innerText);
-a.innerText=counterVal+1});
+// ----Heart button functionality----
+heart.addEventListener("click", () => {
+  // 1 has been liked <span>4</span> times
 
+  const currentNumber = counter.textContent;
+  const likeElement = document.getElementById(`likes-${currentNumber}`)
+  const likesContainer = document.querySelector("ul.likes");
 
-document.getElementById("heart").addEventListener
-document.getElementById("pause").addEventListener
+  if (!likeElement) {
+    const li = document.createElement("li");
+    // span will translate if using innerHTML instead of textContent
+    const string = `${currentNumber} has been liked <span>1</span> times`;
 
+    li.id = `likes-${currentNumber}`;
+    li.innerHTML = string;
+
+    likesContainer.append(li);
+  } else {
+      const innerSpanElement = likeElement.querySelector("span")
+      const currentLikeNumberAsAString = innerSpanElement.textContent
+      const likeNumber = parseInt(currentLikeNumberAsAString)
+      innerSpanElement.textContent = likeNumber + 1
+  }
+})
+
+// ----Comment submission functionality----
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const userInput = e.target.comment.value;
+  const p = document.createElement('p');
+
+  p.textContent = userInput;
+  commentsContainer.append(p);
+
+  e.target.reset();
+})
+
+// ----Functionality for Timer and Pause Button----
+const pause = document.getElementById("pause");
+let paused = false;
+
+// The return value of setInterval is a numeric identifier of
+// the current timer's value. This can be used to pause
+// the timed increments
+let timerID = setInterval(() => {
+  increment();
+}, 1000)
